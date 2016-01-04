d680acd53980fe5b665bccad6f05082d465b9011
张莉 <287081764@qq.com>
2016-01-04 11:23:37 +0800

    first

---
+++ HiWeekend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HiWeekend
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 scjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
